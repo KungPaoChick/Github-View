cf622a999f507555f7fab925396b2cd53fe4536e
KungPaoChick <matthewriel63@gmail.com>
2021-03-22 14:56:48 +0000

    📦 Added feat: description of repository will be written in .txt

--- cloner.py
+++ cloner.py
@@ -8,10 +8,13 @@ from bs4 import BeautifulSoup as soup
 
 def repos(username, links):
     with open(os.path.join(name, f'{username}.csv'), 'w', encoding='utf-8') as f:
-        headers = ['Link', 'Repository', 'Stars', 'Forks']
+        headers = ['Link', 'Repository', 'Stars', 'Forks', 'Contributors']
         writer = csv.writer(f, dialect='excel')
         
         writer.writerow(headers)
+        print(colorama.Fore.YELLOW,
+            f'[!] {name} has {len(links)} Public Repositor{plural_ies(len(links))}\n',
+            colorama.Style.RESET_ALL)
         for link in links:
             with requests.get(link) as rep_response:
                 rep_response.raise_for_status()
@@ -23,7 +26,17 @@ def repos(username, links):
                 print(colorama.Fore.GREEN,
                     f'[*] {repo_name.text}', colorama.Style.RESET_ALL)
                 my_data.append(repo_name.text)
-            
+
+            # gets description of the repository
+            with open(os.path.join(name, f'{repo_name.text}.txt'), 'w') as repo_des:
+                abouts = {'None' : [x.text for x in rep_soup.findAll('div', {'class': 'f4 mt-3 color-text-secondary text-italic'})],
+                          'About' : [x.text for x in rep_soup.findAll('p', {'class': 'f4 mt-3'})]}
+                if abouts['About'] == []:
+                    os.remove(os.path.join(name, f'{repo_name.text}.txt'))
+                else:
+                    for info in abouts['About']:
+                        repo_des.write(info)
+
             # gets star count
             for star in [x.text.split() for x in rep_soup.findAll('a', {'href': f'{link.split(url)[1]}/stargazers'})].pop(0):
                 my_data.append(star)
@@ -32,7 +45,19 @@ def repos(username, links):
             for fork in [x.text.split() for x in rep_soup.findAll('a', {'href': f'{link.split(url)[1]}/network/members'})].pop(0):
                 my_data.append(fork)
 
+            con = []
+            for contributor in [x['title'] for x in rep_soup.findAll('span', {'class': 'Counter'})]:
+                con.append(contributor)
+
+            if int(con[-1]) == 0:
+                my_data.append(1)
+            else:
+                my_data.append(con[-1])
             writer.writerows([my_data])
+
+
+def plural_ies(v):
+    return 'ies' if not abs(v) == 1 else 'y'
 
 
 def conn(uname, main_url):
