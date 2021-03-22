4a87168ea7254d78071342049e3166efd20cef0f
KungPaoChick <matthewriel63@gmail.com>
2021-03-22 13:44:08 +0000

    📦 Added feat: write to csv to view all repositories and info

--- cloner.py
+++ cloner.py
@@ -1,4 +1,3 @@
-import formatter
 import requests
 import argparse
 import colorama
@@ -7,18 +6,51 @@ import csv
 from bs4 import BeautifulSoup as soup
 
 
-def conn(uname):
-    with requests.get(f'https://github.com/{uname}?tab=repositories') as response:
+def repos(username, links):
+    with open(os.path.join(name, f'{username}.csv'), 'w', encoding='utf-8') as f:
+        headers = ['Link', 'Repository', 'Stars', 'Forks']
+        writer = csv.writer(f, dialect='excel')
+        
+        writer.writerow(headers)
+        for link in links:
+            with requests.get(link) as rep_response:
+                rep_response.raise_for_status()
+                rep_soup = soup(rep_response.text, 'html.parser')
+
+            my_data = [link]
+            # gets repo name
+            for repo_name in rep_soup.findAll('a', {'data-pjax':'#js-repo-pjax-container'}):
+                print(colorama.Fore.GREEN,
+                    f'[*] {repo_name.text}', colorama.Style.RESET_ALL)
+                my_data.append(repo_name.text)
+            
+            # gets star count
+            for star in [x.text.split() for x in rep_soup.findAll('a', {'href': f'{link.split(url)[1]}/stargazers'})].pop(0):
+                my_data.append(star)
+
+            # gets fork count
+            for fork in [x.text.split() for x in rep_soup.findAll('a', {'href': f'{link.split(url)[1]}/network/members'})].pop(0):
+                my_data.append(fork)
+
+            writer.writerows([my_data])
+
+
+def conn(uname, main_url):
+    with requests.get(f'{main_url}/{uname}?tab=repositories') as response:
         response.raise_for_status()
         page_soup = soup(response.text, 'html.parser')
 
-    for repo in page_soup.findAll('a', {'itemprop': 'name codeRepository'}):
-        print(repo.text)
+        links = []
+        for repo in page_soup.findAll('a', {'itemprop': 'name codeRepository'}):
+            links.append(f"{main_url}{repo['href']}")
+
+        for name in page_soup.findAll('span', {'itemprop':'name'}):
+            return repos(name.text, links)
 
 
 if __name__ == '__main__':
     colorama.init()
-
+    url = 'https://github.com'
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                      description="Clones all Public Repositories from Github Users.")
 
@@ -30,4 +62,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for name in args.username:
-        conn(name)
+        if not os.path.exists(name):
+            os.mkdir(name)
+        conn(name, url)
