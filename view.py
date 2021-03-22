8495720d83abbbd147ecb9503252329d791b30a3
KungPaoChick <matthewriel63@gmail.com>
2021-03-22 21:13:55 +0000

    🐛 Fixed few bugs, added try/except when handling requests, and created function for creating dataframe

--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+from pynvim import encoding
 import requests
 import argparse
 import colorama
@@ -9,55 +10,68 @@ from bs4 import BeautifulSoup as soup
 
 def repos(username, links):
     with open(os.path.join(name, f'{username}.csv'), 'w', encoding='utf-8') as f:
-        headers = ['Link', 'Repository', 'Commits', 'Stars', 'Forks', 'Contributors']
+        headers = ['Link', 'Repository', 'Commits',
+                   'Stars', 'Forks', 'Contributors']
         writer = csv.writer(f, dialect='excel')
 
         writer.writerow(headers)
         print(colorama.Fore.YELLOW,
-            f'[!] {name} has {len(links)} Public Repositor{plural_ies(len(links))}\n',
-            colorama.Style.RESET_ALL)
+              f'[!] {name} has {len(links)} Public Repositor{plural_ies(len(links))}\n',
+              colorama.Style.RESET_ALL)
         for link in links:
-            with requests.get(link) as rep_response:
-                rep_response.raise_for_status()
-                rep_soup = soup(rep_response.text, 'html.parser')
+            try:
+                with requests.get(link, timeout=1) as rep_response:
+                    rep_response.raise_for_status()
+                    rep_soup = soup(rep_response.text, 'html.parser')
 
-            my_data = [link]
-            # gets repo name
-            for repo_name in rep_soup.findAll('a', {'data-pjax':'#js-repo-pjax-container'}):
-                print(colorama.Fore.GREEN,
-                    f'[*] {repo_name.text}', colorama.Style.RESET_ALL)
-                my_data.append(repo_name.text)
+                my_data = [link]
+                # gets repo name
+                for repo_name in rep_soup.findAll('a', {'data-pjax': '#js-repo-pjax-container'}):
+                    print(colorama.Fore.GREEN,
+                          f'[*] {repo_name.text}', colorama.Style.RESET_ALL)
+                    my_data.append(repo_name.text)
 
-            # gets number of commits to the repository
-            my_data.append([x.text.split() for x in rep_soup.findAll('ul', {'class': 'list-style-none d-flex'})][0].pop(0))
+                # gets number of commits to the repository
+                my_data.append([x.text.split() for x in rep_soup.findAll(
+                    'ul', {'class': 'list-style-none d-flex'})][0].pop(0))
 
-            # gets description of the repository
-            with open(os.path.join(name, f'{repo_name.text}.txt'), 'w') as repo_des:
-                abouts = {'None' : [x.text for x in rep_soup.findAll('div', {'class': 'f4 mt-3 color-text-secondary text-italic'})],
-                          'About' : [x.text for x in rep_soup.findAll('p', {'class': 'f4 mt-3'})]}
-                if abouts['About'] == []:
-                    os.remove(os.path.join(name, f'{repo_name.text}.txt'))
+                # gets description of the repository
+                with open(os.path.join(name, f'{repo_name.text}.txt'), 'w') as repo_des:
+                    abouts = {'None': [x.text for x in rep_soup.findAll('div', {'class': 'f4 mt-3 color-text-secondary text-italic'})],
+                              'About': [x.text for x in rep_soup.findAll('p', {'class': 'f4 mt-3'})]}
+                    if abouts['About'] == []:
+                        os.remove(os.path.join(name, f'{repo_name.text}.txt'))
+                    else:
+                        for info in abouts['About']:
+                            repo_des.write(info)
+
+                # gets star count
+                my_data.append([star.text.split() for star in rep_soup.findAll(
+                    'a', {'href': f'{link.split(url)[1]}/stargazers'})].pop(0)[0])
+
+                # gets fork count
+                my_data.append([fork.text.split() for fork in rep_soup.findAll(
+                    'a', {'href': f'{link.split(url)[1]}/network/members'})].pop(0)[0])
+
+                con = []
+                for contributor in [x['title'] for x in rep_soup.findAll('span', {'class': 'Counter'})]:
+                    con.append(contributor)
+
+                if int(con[-1]) == 0:
+                    my_data.append(1)
                 else:
-                    for info in abouts['About']:
-                        repo_des.write(info)
+                    my_data.append(con[-1])
+                writer.writerows([my_data])
+            except requests.HTTPError as err:
+                print(colorama.Fore.RED,
+                      f'[!!] Something went wrong! {err}',
+                      colorama.Style.RESET_ALL)
 
-            # gets star count
-            my_data.append([star.text.split() for star in rep_soup.findAll('a', {'href': f'{link.split(url)[1]}/stargazers'})].pop(0)[0])
+        return read_data(os.path.join(os.getcwd(), name), f'{username}.csv')
 
-            # gets fork count
-            my_data.append([fork.text.split() for fork in rep_soup.findAll('a', {'href': f'{link.split(url)[1]}/network/members'})].pop(0)[0])
 
-            con = []
-            for contributor in [x['title'] for x in rep_soup.findAll('span', {'class': 'Counter'})]:
-                con.append(contributor)
-
-            if int(con[-1]) == 0:
-                my_data.append(1)
-            else:
-                my_data.append(con[-1])
-            writer.writerows([my_data])
-
-    df = pd.read_csv(os.path.join(os.getcwd(), name, f'{username}.csv'))
+def read_data(path, filename):
+    df = pd.read_csv(os.path.join(path, filename), encoding='utf-8')
     pd.set_option('display.max_rows', None)
     df.drop(['Link'], axis=1, inplace=True)
     print(df)
@@ -68,16 +82,21 @@ def plural_ies(v):
 
 
 def conn(uname, main_url):
-    with requests.get(f'{main_url}/{uname}?tab=repositories') as response:
-        response.raise_for_status()
-        page_soup = soup(response.text, 'html.parser')
+    try:
+        with requests.get(f'{main_url}/{uname}?tab=repositories') as response:
+            response.raise_for_status()
+            page_soup = soup(response.text, 'html.parser')
 
-        links = []
-        for repo in page_soup.findAll('a', {'itemprop': 'name codeRepository'}):
-            links.append(f"{main_url}{repo['href']}")
+            links = []
+            for repo in page_soup.findAll('a', {'itemprop': 'name codeRepository'}):
+                links.append(f"{main_url}{repo['href']}")
 
-        for name in page_soup.findAll('span', {'itemprop':'name'}):
-            return repos(name.text, links)
+            for name in page_soup.findAll('span', {'itemprop': 'name'}):
+                return repos(name.text, links)
+    except requests.HTTPError as err:
+        print(colorama.Fore.RED,
+              f'[!!] Something went wrong! {err}',
+              colorama.Style.RESET_ALL)
 
 
 if __name__ == '__main__':
